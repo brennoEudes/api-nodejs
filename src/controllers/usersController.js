84977ada221355e588e9ffa9abe6ecb5b18131f8
“Brenno <dev.brennoeudes@gmail.com>
2024-04-04 18:07:02 -0300

    usersController config

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,15 +1,25 @@
+const { hash, compare } = require("bcryptjs");
+const { response } = require("express");
+const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 
 class UsersController {
-  // Cria novo usuário:
-  create(req, res) {
-    const { name, email, password } = req.body;
 
-    if (!name) {
-      throw new AppError("Name is required!");
+  // Cria usuário:
+  async create(req, res) {
+    const { name, email, password, avatar } = req.body;
+
+    const checkIfUserExist = await knex("users").where("email", email).first();
+
+    if (checkIfUserExist) {
+      throw new AppError("Email has been used!");
     }
 
-    res.status(201).json({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({name, email, password:hashedPassword, avatar})
+
+    return res.status(201).json("User created sucessfully!");
   }
 }
 
